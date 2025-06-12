dddfa3c77fdffd3c09704c1ae3452cac045ffe4f
Connor Denihan <cdenihan@proton.me>
2025-06-12 10:29:24 -0400

    Add data visualization setup script
    Implement a script to set up a data visualization environment using Foxglove or Plotjuggler. The script includes commands to start PX4, Gazebo, QGroundControl, and the ROS2 MAVROS node, allowing users to choose between Foxglove and Plotjuggler for visualization.

---
+++ main.py
@@ -0,0 +1,51 @@
+import argparse
+import subprocess
+import sys
+import os
+
+choice = 0;
+
+# This script sets up a data visualization environment using either Foxglove or Plotjuggler.
+
+def run_cmd(cmd):
+    subprocess.Popen(
+        cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        preexec_fn=os.setsid,
+        shell=True
+    )
+
+
+def run_setup():
+    print("Initiating Data Visualization Setup...")
+    # Start PX4 and Gazebo
+    run_cmd(["cd", "~/PX4-Autopilot/", "&&", "make", "px4_sitl", "gz_x500"])
+    # Start QGroundControl
+    run_cmd(["cd", "~", "&&", "./QGroundControl-x86_64.AppImage"])
+    # Start ROS2 MAVROS node
+    run_cmd(["ros2", "run", "mavros", "mavros_node", "--ros-args", "-p", "fcu_url:=udp://:14540@127.0.1:14557", "-p", "target_component_id:=1", "-r", "__ns:=/mavros"])
+    # Start Foxglove or Plotjuggler
+    if choice == 1:
+        run_cmd(["ros2", "launch", "foxglove_bridge", "foxglove_bridge_launch.xml"])
+        run_cmd(["foxglove-studio", '"foxglove://open?ds=foxglove-websocket&ds.url=ws://localhost:8765/"'])
+    elif choice == 2:
+        run_cmd(["plotjuggler"])
+
+def main():
+    parser = argparse.ArgumentParser(description="Choose a mode to run the script.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--foxglove", action="store_true", help="Run in file mode")
+    group.add_argument("-p", "--plotjuggler", action="store_true", help="Run in process mode")
+
+    args = parser.parse_args()
+
+    if args.foxglove:
+        choice = 1
+    elif args.plotjuggler:
+        choice = 2
+
+    run_setup()
+
+if __name__ == "__main__":
+    main()
