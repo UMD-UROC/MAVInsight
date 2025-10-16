6a204bc9149cf394d52e3a174f437d4ccc77e226
JSchmucki <joshuaschmucki@gmail.com>
2025-10-16 11:23:26 -0400

    split out sensor unittests into own files

---
+++ test/camera_test.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+from models.sensor import Camera, sensor_factory
+from models.sensor_types import SensorTypes
+
+class TestCamera(unittest.TestCase):
+    def setUp(self):
+        self.share_dir = os.path.dirname(__file__)
+
+        self.sensor_dir = os.path.join(self.share_dir, "sensors")
+
+        self.cm_dir = os.path.join(self.sensor_dir, "cameras")
+
+        self.full_camera:Camera = sensor_factory(os.path.join(self.cm_dir, "full_camera.yaml"))
+
+    def test_sensor_factory(self):
+        """sensor factory correctly makes camera"""
+        self.assertIsInstance(self.full_camera, Camera)
+
+    def test_camera_from_yaml(self):
+        """camera creation from yaml"""
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_no_name.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_no_frame.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_no_parent.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_no_topic.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_no_type.yaml"))
+
+        # offset cases
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_long_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_short_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_none_in_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.cm_dir, "cm_bad_offset.yaml"))
+
+        cm0 = sensor_factory(os.path.join(self.cm_dir, "cm_no_offset.yaml"))
+        self.assertEqual(cm0.offset[0], 0.0)
+        self.assertEqual(cm0.offset[1], 0.0)
+        self.assertEqual(cm0.offset[2], 0.0)
+
+        self.assertEqual(self.full_camera.name, "Full test cam")
+        self.assertEqual(self.full_camera.frame_name, "test_cam")
+        self.assertEqual(len(self.full_camera.offset), 3)
+        self.assertEqual(self.full_camera.offset[0], 9.0)
+        self.assertEqual(self.full_camera.offset[1], 8.0)
+        self.assertEqual(self.full_camera.offset[2], 7.6)
+        self.assertEqual(self.full_camera.cam_info_topic, "/test/cam")
+        self.assertEqual(self.full_camera.parent_frame, "link")
+        self.assertEqual(self.full_camera.sensor_type, SensorTypes.CAMERA)
---
+++ test/rangefinder_test.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+from models.sensor import Rangefinder, sensor_factory
+from models.sensor_types import SensorTypes
+
+class TestRangefinder(unittest.TestCase):
+    def setUp(self):
+        self.share_dir = os.path.dirname(__file__)
+
+        self.sensor_dir = os.path.join(self.share_dir, "sensors")
+
+        self.rf_dir = os.path.join(self.sensor_dir, "rangefinders")
+
+        self.full_rangefinder:Rangefinder = sensor_factory(os.path.join(self.rf_dir, "full_rangefinder.yaml"))
+
+    def test_sensor_factory(self):
+        """sensor factory correctly makes rangefinder"""
+        self.assertIsInstance(self.full_rangefinder, Rangefinder)
+
+    def test_rangefinder_from_yaml(self):
+        """rangefinder creation from yaml"""
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_no_name.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_no_frame.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_no_parent.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_no_topic.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_no_type.yaml"))
+
+        # offset cases
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_long_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_short_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_none_in_offset.yaml"))
+        self.assertRaises(ValueError, sensor_factory, os.path.join(self.rf_dir, "rf_bad_offset.yaml"))
+
+        rf0 = sensor_factory(os.path.join(self.rf_dir, "rf_no_offset.yaml"))
+        self.assertEqual(rf0.offset[0], 0.0)
+        self.assertEqual(rf0.offset[1], 0.0)
+        self.assertEqual(rf0.offset[2], 0.0)
+
+        self.assertEqual(self.full_rangefinder.name, "test full rangefinder")
+        self.assertEqual(self.full_rangefinder.frame_name, "test_rangefinder")
+        self.assertEqual(len(self.full_rangefinder.offset), 3)
+        self.assertEqual(self.full_rangefinder.offset[0], 1.0)
+        self.assertEqual(self.full_rangefinder.offset[1], 2.0)
+        self.assertEqual(self.full_rangefinder.offset[2], 3.0)
+        self.assertEqual(self.full_rangefinder.range_topic, "/test/rangefinder")
+        self.assertEqual(self.full_rangefinder.parent_frame, "link")
+        self.assertEqual(self.full_rangefinder.sensor_type, SensorTypes.RANGEFINDER)
